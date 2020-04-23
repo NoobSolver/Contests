4cb3be2385e8a0477b0aad2473cae408e313eca1
Gaurav Kumar Mathur <mathuranant123@gmail.com>
2020-04-23 12:39:36 +0530

    Solution added

--- Practice/C3PRB2.cpp
+++ Practice/C3PRB2.cpp
@@ -1,10 +1,5 @@
-/******************************************
-*  AUTHOR:         ABHISHEK YADAV         *
-*  INSTITUITION:   CHANDIGARH UNIVERSITY  *
-******************************************/
 #include "bits/stdc++.h"
-	using namespace std;
-	
+using namespace std;
 #define io std::ios::sync_with_stdio(false);cin.tie(NULL);
 #define FOR(i,n) for(ll i = 0 ; i < n ; i++)
 #define FORR(i,a,b) for(ll i = a ; i < b ; i++)
@@ -22,12 +17,12 @@
 #define inf 1000000000000
 #define PI 3.1415926535897932384626433832795
 #define MOD 1000000007
- 
+Â 
 template<typename T, typename U> inline void umin(T &x, U y) { if(y < x) x = y; }
 template<typename T, typename U> inline void umax(T &x, U y) { if(x < y) x = y; }
- 
- 
- 
+Â 
+Â 
+Â 
 int main(){
 	io
 	#ifdef ABHI
